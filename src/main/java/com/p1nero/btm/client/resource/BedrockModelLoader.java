8341db7c308891ced67a6f3f00518a20df099358
p1nero <1070402601@qq.com>
2026-08-11 11:05:29 +0800

    Use custom trident inventory texture on Forge

--- src/main/java/com/p1nero/btm/client/resource/BedrockModelLoader.java
+++ src/main/java/com/p1nero/btm/client/resource/BedrockModelLoader.java
@@ -7,6 +7,7 @@ import com.github.mcmodderanchor.simplebedrockmodel.v1.resource.RawResourceLoade
 import com.p1nero.btm.BetterTridentModelMod;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
@@ -16,6 +17,7 @@ import java.util.Objects;
 public final class BedrockModelLoader {
     public static final ResourceLocation TRIDENT = id("entity/trident");
     public static final ResourceLocation TRIDENT_TEXTURE = id("textures/entity/trident.png");
+    public static final ResourceLocation TRIDENT_INVENTORY_MODEL = id("item/trident_inventory");
 
     private static BedrockModel trident;
 
@@ -30,6 +32,11 @@ public final class BedrockModelLoader {
     @SubscribeEvent
     public static void captureModel(RegisterBedrockModelReloadListenerEvent event) {
         event.register(models -> trident = models.get(TRIDENT));
+    }
+
+    @SubscribeEvent
+    public static void registerInventoryModel(ModelEvent.RegisterAdditional event) {
+        event.register(TRIDENT_INVENTORY_MODEL);
     }
 
     public static BedrockModel getTridentModel() {
---
+++ src/main/java/com/p1nero/btm/mixin/ItemRendererMixin.java
@@ -0,0 +1,42 @@
+package com.p1nero.btm.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.p1nero.btm.client.resource.BedrockModelLoader;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.client.resources.model.ModelManager;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ItemRenderer.class)
+public class ItemRendererMixin {
+    @Redirect(
+            method = "render(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/item/ItemDisplayContext;ZLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;IILnet/minecraft/client/resources/model/BakedModel;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/resources/model/ModelManager;getModel(Lnet/minecraft/client/resources/model/ModelResourceLocation;)Lnet/minecraft/client/resources/model/BakedModel;",
+                    ordinal = 0
+            )
+    )
+    private BakedModel better_trident_model$useInventoryModel(
+            ModelManager modelManager,
+            ModelResourceLocation originalModel,
+            ItemStack stack,
+            ItemDisplayContext displayContext,
+            boolean leftHand,
+            PoseStack poseStack,
+            MultiBufferSource buffers,
+            int packedLight,
+            int packedOverlay,
+            BakedModel model
+    ) {
+        return displayContext == ItemDisplayContext.GUI
+                ? modelManager.getModel(BedrockModelLoader.TRIDENT_INVENTORY_MODEL)
+                : modelManager.getModel(originalModel);
+    }
+}
